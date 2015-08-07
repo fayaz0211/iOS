50a4e8c834b95664af30258856ff31b8da93b7f3
fayaz <fayaz@fayazs-MacBook-Pro.local>
2015-08-07 15:55:39 +0100

    Initial Commit

---
+++ twitter-project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitter-project
+//
+//  Created by fayaz on 07/08/2015.
+//  Copyright (c) 2015 fayaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twitter-project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twitter-project
+//
+//  Created by fayaz on 07/08/2015.
+//  Copyright (c) 2015 fayaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
